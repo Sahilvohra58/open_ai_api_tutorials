234f79c703b886cde9bcf809d231353c2e69bab0
Sahil Vohra <sahil.vohra@loblaw.ca>
2024-08-07 18:09:30 -0400

    Embedding API parts added

---
+++ 8 Embeddings API/utils/embedding_utils.py
@@ -0,0 +1,12 @@
+import numpy as np
+from openai import OpenAI
+client = OpenAI()
+
+
+def get_embedding(text, model="text-embedding-3-small"):
+   text = text.replace("\n", " ")
+   return client.embeddings.create(input = [text], model=model).data[0].embedding
+
+
+def cosine_similarity(a, b):
+    return np.dot(a, b) / (np.linalg.norm(a) * np.linalg.norm(b))
